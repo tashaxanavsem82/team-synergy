1f8b72df435e45601c6e5556dac3ee7e979ebcb1
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-02 11:22:26 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Team Synergy!');
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
